090af0f2eacabc459a3e6642a4b2843cb10ebff3
John Aziz <johnaziz269@gmail.com>
2024-03-06 18:04:56 +0200

    format with ruff and black (#14)

--- markdown_checker/validate_markdown.py
+++ markdown_checker/validate_markdown.py
@@ -8,6 +8,7 @@ from markdown_checker.check_markdown import check_broken_links
 from markdown_checker.paths.files_paths_reader import get_files_paths_list
 from markdown_checker.generators.md_generator import generate_md
 
+
 def main() -> None:
     """Main program get inputs and run checks"""
 
@@ -16,7 +17,14 @@ def main() -> None:
 
     _, files_paths = get_files_paths_list(in_arg.dir)
 
-    pass_list = ['./CODE_OF_CONDUCT.md', './CONTRIBUTING.md', './SECURITY.md', '.\CODE_OF_CONDUCT.md', '.\CONTRIBUTING.md', '.\SECURITY.md']
+    pass_list = [
+        "./CODE_OF_CONDUCT.md",
+        "./CONTRIBUTING.md",
+        "./SECURITY.md",
+        ".\CODE_OF_CONDUCT.md",
+        ".\CONTRIBUTING.md",
+        ".\SECURITY.md",
+    ]
     files_paths = [file_path for file_path in files_paths if file_path not in pass_list]
 
     formatted_output = ""
@@ -24,25 +32,28 @@ def main() -> None:
     # iterate over the files to validate the content
     for file_path in files_paths:
         if "check_broken_paths" in in_arg.func:
-            broken_paths = check_broken_links(file_path, "path" , "broken")
+            broken_paths = check_broken_links(file_path, "path", "broken")
             if broken_paths:
                 formatted_output += broken_paths
         if "check_paths_tracking" in in_arg.func:
-            paths_tracking = check_broken_links(file_path, "path" , "tracking")
+            paths_tracking = check_broken_links(file_path, "path", "tracking")
             if paths_tracking:
                 formatted_output += paths_tracking
         if "check_urls_tracking" in in_arg.func:
-            urls_tracking = check_broken_links(file_path, "url" , "tracking")
+            urls_tracking = check_broken_links(file_path, "url", "tracking")
             if urls_tracking:
                 formatted_output += urls_tracking
         if "check_urls_locale" in in_arg.func:
-            urls_locale = check_broken_links(file_path, "url" , "locale")
+            urls_locale = check_broken_links(file_path, "url", "locale")
             if urls_locale:
                 formatted_output += urls_locale
     if formatted_output != "":
-        formatted_output = "| File Full Path | Issues |\n|--------|--------|\n" + formatted_output
+        formatted_output = (
+            "| File Full Path | Issues |\n|--------|--------|\n" + formatted_output
+        )
         print(formatted_output)
         generate_md(formatted_output, in_arg.func, in_arg.guide_url)
+
 
 if __name__ == "__main__":
     main()
--- tests/test_markdown_checker.py
+++ tests/test_markdown_checker.py
@@ -4,38 +4,99 @@ Module to check paths in markdown files.
 
 import subprocess
 
+
 def check_broken_paths():
     """
     Function to check for broken paths.
     """
     print("check_broken_paths was called")
-    subprocess.run(["markdown-checker", "-d", "./tests/resources/", "-f", "check_broken_paths", "-gu", "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md"], check=False)
+    subprocess.run(
+        [
+            "markdown-checker",
+            "-d",
+            "./tests/resources/",
+            "-f",
+            "check_broken_paths",
+            "-gu",
+            "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md",
+        ],
+        check=False,
+    )
+
 
 def check_paths_tracking():
     """
     Function to check paths tracking.
     """
     print("check_paths_tracking was called")
-    subprocess.run(["markdown-checker", "-d", "./tests/resources/", "-f", "check_paths_tracking", "-gu", "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md"], check=False)
+    subprocess.run(
+        [
+            "markdown-checker",
+            "-d",
+            "./tests/resources/",
+            "-f",
+            "check_paths_tracking",
+            "-gu",
+            "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md",
+        ],
+        check=False,
+    )
+
 
 def check_urls_tracking():
     """
     Function to check URLs tracking.
     """
     print("check_urls_tracking was called")
-    subprocess.run(["markdown-checker", "-d", "./tests/resources/", "-f", "check_urls_tracking", "-gu", "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md"], check=False)
+    subprocess.run(
+        [
+            "markdown-checker",
+            "-d",
+            "./tests/resources/",
+            "-f",
+            "check_urls_tracking",
+            "-gu",
+            "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md",
+        ],
+        check=False,
+    )
+
 
 def check_urls_locale():
     """
     Function to check URLs locale.
     """
     print("check_urls_locale was called")
-    subprocess.run(["markdown-checker", "-d", "./tests/resources/", "-f", "check_urls_locale", "-gu", "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md"], check=False)
+    subprocess.run(
+        [
+            "markdown-checker",
+            "-d",
+            "./tests/resources/",
+            "-f",
+            "check_urls_locale",
+            "-gu",
+            "https://github.com/john0isaac/markdown-checker/blob/main/CONTRIBUTING.md",
+        ],
+        check=False,
+    )
 
-mydict = {1:check_broken_paths, 2:check_paths_tracking, 3:check_urls_tracking, 4:check_urls_locale}
+
+mydict = {
+    1: check_broken_paths,
+    2: check_paths_tracking,
+    3: check_urls_tracking,
+    4: check_urls_locale,
+}
 
 try:
-    choices = list(map(int,input("Do you want to: \n(1) Run check_broken_paths \n(2) Run check_paths_tracking \n(3) Run check_urls_tracking \n(4) Run check_urls_locale \n").split()))
+    choices = list(
+        map(
+            int,
+            input(
+                "Do you want to: \n(1) Run check_broken_paths \n(2) Run check_paths_tracking \n(3) Run check_urls_tracking \n(4) Run check_urls_locale \n"
+            ).split(),
+        )
+    )
 except ValueError:
     print("Please input number")
 
