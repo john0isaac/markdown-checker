a25e0a2a9ca977897c38c687b588988fd427d77b
John Aziz <johnaziz269@gmail.com>
2024-08-11 03:19:56 +0300

    fix tracemalloc errors (#69)

--- src/markdown_checker/reports/md_reports/generator.py
+++ src/markdown_checker/reports/md_reports/generator.py
@@ -6,24 +6,19 @@ from markdown_checker.reports.generator_base import GeneratorBase
 
 class MarkdownGenerator(GeneratorBase):
     current_dir = pathlib.Path(__file__).parent
-    check_broken_paths_template = open(current_dir / "templates/paths/broken.md").read()
-    check_broken_urls_template = open(current_dir / "templates/urls/broken.md").read()
-    check_paths_tracking_template = open(current_dir / "templates/paths/tracking.md").read()
-    check_urls_tracking_template = open(current_dir / "templates/urls/tracking.md").read()
-    check_urls_locale_template = open(current_dir / "templates/urls/locale.md").read()
-    templates = {
-        "check_broken_paths": check_broken_paths_template,
-        "check_broken_urls": check_broken_urls_template,
-        "check_paths_tracking": check_paths_tracking_template,
-        "check_urls_tracking": check_urls_tracking_template,
-        "check_urls_locale": check_urls_locale_template,
-    }
 
     def __init__(
         self,
         contributing_guide_url: Union[str, None] = None,
         output_file_name: str = "comment",
     ) -> None:
+        self.templates = {
+            "check_broken_paths": open(self.current_dir / "templates/paths/broken.md").read(),
+            "check_broken_urls": open(self.current_dir / "templates/urls/broken.md").read(),
+            "check_paths_tracking": open(self.current_dir / "templates/paths/tracking.md").read(),
+            "check_urls_tracking": open(self.current_dir / "templates/urls/tracking.md").read(),
+            "check_urls_locale": open(self.current_dir / "templates/urls/locale.md").read(),
+        }
         self.output_file_name = output_file_name
         self.contributing_guide_line = (
             (f" For more details, check our [Contributing Guide]({contributing_guide_url}).\n\n")
